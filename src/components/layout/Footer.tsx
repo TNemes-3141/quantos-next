a5c6b294f46e61d6b1d250d567f01f97965569bc
Tamas Nemes <nemestomi2@gmail.com>
2024-05-03 23:09:47 +0200

    Make footer responsive

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
     return (
         <div>
             <Separator />
-            <footer className="grid grid-cols-2 px-24 py-12">
+            <footer className="grid max-sm:grid-rows-2 md:grid-cols-2 px-24 py-12 gap-6">
                 <div className="flex flex-col justify-start items-left gap-4">
                     <Link href="https://quantos.online/">
                         <Image
@@ -18,9 +18,11 @@ export default function Footer() {
                             height={40}
                         />
                     </Link>
-                    <p className="text-muted-foreground">© 2024 Tamas Nemes. Made with ❤️</p>
+                    <p className="text-muted-foreground">© 2024 Tamas Nemes. Made with ❤️ with support from&nbsp;
+                        <Link href="https://www.fz-juelich.de/en/ias/jsc" className="underline">Jülich Supercomputing Centre</Link>
+                    </p>
                 </div>
-                <div className="flex justify-end items-center gap-4">
+                <div className="flex justify-center md:justify-end items-center gap-4">
                     <p>Sprache:</p>
                     <LanguageSelector />
                 </div>
