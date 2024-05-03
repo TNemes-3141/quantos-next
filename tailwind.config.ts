4a632a4719a3bde3c2c345e39edb5c269260721b
Tamas Nemes <nemestomi2@gmail.com>
2024-05-03 16:13:56 +0200

    Add definition of fade-in animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,10 +27,21 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fadeIn": {
+          from: {
+            opacity: "0",
+            transform: "translateY(16px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fadeIn": "fadeIn 0.3s ease-in-out forwards",
       },
       colors: {
         background: "hsl(var(--background))",
