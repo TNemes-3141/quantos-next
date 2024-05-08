e7f1bd6fdc7b1811018b115dd555adde90f448ff
Tamas Nemes <nemestomi2@gmail.com>
2024-05-08 16:41:56 +0200

    Add arguments to reworked function

--- src/app/[locale]/db-test/page.tsx
+++ src/app/[locale]/db-test/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     const post = async () => {
         const supabase = createClient();
 
-        const { data, error } = await supabase.rpc('generate_random_code');
+        const { data, error } = await supabase.rpc('generate_random_code', { team_name: "Fraunhofer TTO", expiry: 60, num_codes: 5 });
         console.log(data);
     }
 
