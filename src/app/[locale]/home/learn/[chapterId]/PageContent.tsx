0b84d4d215cc8bcacb8778b58290816c77056e44
Tamas Nemes <nemestomi2@gmail.com>
2024-05-19 01:51:59 +0200

    Complete chapter page

--- src/app/[locale]/home/learn/[chapterId]/PageContent.tsx
+++ src/app/[locale]/home/learn/[chapterId]/PageContent.tsx
@@ -1,33 +1,87 @@
 import LessonSelectionCarousel from "@/components/LessonSelectionCarousel";
 import BackButton from "@/components/BackButton";
 import LearnBreadcrumbs from "@/components/LearnBreadcrumbs";
+import ChapterThumbnailAnimation from "@/components/ChapterThumbnailAnimation";
+import { Badge } from "@/components/shadcn-ui/badge";
+import { Skeleton } from "@/components/shadcn-ui/skeleton";
 
 import { LocalizedProps } from "@/i18n";
 import { cn } from "@/lib/utils";
 import { secondary_font } from "@/lib/fonts";
-import { getChapterName, getLessonCardData } from "./getLessons";
+import { DifficultyLevel } from "@/lib/types";
+import { createClient } from "@/lib/supabase/server";
+import { getChapterData, getLessonCardData } from "./getLessons";
 
 
 type PageContentProps = LocalizedProps & {
     chapterId: string,
 }
 
+function splitToBucketAndPath(input: string): { bucket: string, resourcePath: string | null } {
+    const firstSlashIndex = input.indexOf('/');
+    if (firstSlashIndex === -1) {
+        return { bucket: input, resourcePath: null }; // No slash found, return input as 'before' and empty string as 'after'
+    }
+    const before = input.substring(0, firstSlashIndex);
+    const after = input.substring(firstSlashIndex + 1);
+    return { bucket: before, resourcePath: after };
+}
+
 export default async function PageContent(props: PageContentProps) {
     const lessons = await getLessonCardData(props.chapterId);
-    const chapterTitle = await getChapterName(props.chapterId);
+    const chapterData = await getChapterData(props.chapterId);
+
+    const supabase = createClient();
+    const { bucket, resourcePath } = splitToBucketAndPath(chapterData.thumbnailPath ?? "");
+    let publicUrl: string | null = null;
+    if (chapterData.thumbnailPath && resourcePath) {
+        const { data } = supabase.storage.from(bucket).getPublicUrl(resourcePath);
+        publicUrl = data.publicUrl;
+    }
+
+    const difficultyLevelToLocalizedString = (level: DifficultyLevel): string => {
+        switch (level) {
+            case DifficultyLevel.EASY: return props.translate("learn.difficultyEasy");
+            case DifficultyLevel.ADVANCED: return props.translate("learn.difficultyAdvanced");
+            case DifficultyLevel.CHALLENGING: return props.translate("learn.difficultyChallenging");
+        }
+    }
+
+    const difficultyLevelToVariant = (level: DifficultyLevel): "light" | "medium" | "heavy" => {
+        switch (level) {
+            case DifficultyLevel.EASY: return "light";
+            case DifficultyLevel.ADVANCED: return "medium";
+            case DifficultyLevel.CHALLENGING: return "heavy";
+        }
+    }
 
     return (
         <div className="flex flex-col space-y-6">
             <div className="flex flex-col md:flex-row gap-6 items-left md:items-center">
-                <BackButton label={props.translate("backButtonLabel")}/>
+                <BackButton label={props.translate("backButtonLabel")} />
                 <LearnBreadcrumbs
                     locale={props.locale}
                     translate={props.translate}
                     items={[{
-                        title: chapterTitle,
+                        title: chapterData.title,
                         path: "",
                     }]}
                 />
+            </div>
+            <div className="flex justify-center">
+                {
+                    publicUrl ? <ChapterThumbnailAnimation
+                        size={300}
+                        source={publicUrl}
+                    /> : <Skeleton className="w-full h-full max-w-[300px] max-h-[300px] rounded-xl"/>
+                }
+            </div>
+            <div className="flex flex-col items-center space-y-4">
+                <p className={cn("text-4xl", secondary_font.className)}>{chapterData.title}</p>
+                <Badge variant={difficultyLevelToVariant(chapterData.difficulty)}>
+                    {difficultyLevelToLocalizedString(chapterData.difficulty)}
+                </Badge>
+                <p className="text-center">{chapterData.description}</p>
             </div>
             <p className={cn("text-2xl mt-5", secondary_font.className)}>
                 {props.translate("learn.subheadingLessons")}
--- src/app/[locale]/home/learn/[chapterId]/getLessons.ts
+++ src/app/[locale]/home/learn/[chapterId]/getLessons.ts
@@ -2,7 +2,7 @@
 
 import { eq, and } from "drizzle-orm";
 
-import { ValidLocale } from "@/i18n";
+import { DifficultyLevel } from "@/lib/types";
 import { db } from "@/lib/database/db";
 import { chapters, lessons } from "@/lib/database/schema";
 import { redirect } from "next/navigation";
@@ -12,6 +12,13 @@ export type LessonCardData = {
     id: string,
     title: string,
     readTime: number
+}
+
+export type ChapterData = {
+    title: string,
+    description: string,
+    difficulty: DifficultyLevel,
+    thumbnailPath: string | null,
 }
 
 export async function getLessonCardData(chapterId: string): Promise<LessonCardData[]> {
@@ -29,16 +36,38 @@ export async function getLessonCardData(chapterId: string): Promise<LessonCardDa
     return result;
 }
 
-export async function getChapterName(chapterId: string): Promise<string> {
+export async function getChapterData(chapterId: string): Promise<ChapterData> {
     try {
         const result = await db.query.chapters.findFirst({
             columns: {
                 title: true,
+                description: true,
+                difficulty: true,
+                thumbnailPath: true,
             },
             where: eq(chapters.chapterId, chapterId),
         });
 
-        return result ? result.title : "";
+        if (!result) {
+            redirect("/error");
+        }
+
+        let difficulty: DifficultyLevel;
+        switch (result.difficulty) {
+            case "easy": difficulty = DifficultyLevel.EASY;
+            break;
+            case "advanced": difficulty = DifficultyLevel.ADVANCED;
+            break;
+            case "challenging": difficulty = DifficultyLevel.CHALLENGING;
+            break;
+        }
+
+        return {
+            title: result.title,
+            description: result.description,
+            difficulty: difficulty,
+            thumbnailPath: result.thumbnailPath,
+        }
     } catch (error) {
         redirect("/error");
     }
