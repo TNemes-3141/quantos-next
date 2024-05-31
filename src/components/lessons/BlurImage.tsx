c8bbf5c3275ae7a444e9696cd70342dfd596bdf7
Tamas Nemes <nemestomi2@gmail.com>
2024-06-01 01:47:27 +0200

    Implement aspect ratio component for optimization

--- src/components/lessons/BlurImage.tsx
+++ src/components/lessons/BlurImage.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import { AspectRatio } from "../shadcn-ui/aspect-ratio";
 
 import { cn } from "@/lib/utils";
 
@@ -15,21 +16,23 @@ export default function BlurImage({ src, alttext }: BlurImageProps) {
     const [isLoading, setLoading] = useState(true);
 
     return (
-        <div className="w-[250px] h-[250px] md:w-[400px] md:h-[400px] bg-container rounded-lg overflow-hidden relative">
-            <Image
-                src={src}
-                alt={alttext}
-                fill={true}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                style={{objectFit: "contain"}}
-                className={cn(
-                    'group-hover:opacity-75 duration-700 ease-in-out',
-                    isLoading
-                        ? 'grayscale blur-xl scale-110'
-                        : 'grayscale-0 blur-0 scale-100'
-                )}
-                onLoad={()=> setLoading(false)}
-            />
+        <div className="w-[250px] md:w-[400px] bg-container rounded-lg overflow-hidden relative">
+            <AspectRatio ratio={1} className="relative">
+                <Image
+                    src={src}
+                    alt={alttext}
+                    fill={true}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    style={{ objectFit: "contain" }}
+                    className={cn(
+                        'group-hover:opacity-75 duration-700 ease-in-out',
+                        isLoading
+                            ? 'grayscale blur-xl scale-110'
+                            : 'grayscale-0 blur-0 scale-100'
+                    )}
+                    onLoad={() => setLoading(false)}
+                />
+            </AspectRatio>
         </div>
     );
 }
